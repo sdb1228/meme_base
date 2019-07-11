ea870eb9f80c1c356f547cbb8feb5c30003f0a9b
Cameron Matheson <cameron.matheson@gmail.com>
2019-07-11 13:07:30 -0700

    add absinthe

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule MemeBase.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.4.0"},
+      {:absinthe_plug, "~> 1.4.0"},
     ]
   end
 
