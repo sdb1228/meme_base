861235308bf00a449e44ab0c018b9b5301e7ab8b
Steven Burnett <sburnett@instructure.com>
2019-07-11 15:39:48 -0700

    assets

---
+++ assets/js/Course.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Course = props => (
+  <div key={props.course.id}>
+    <p>{`${props.course.title} by ${props.course.author}`}</p>
+  </div>
+);
+export default Course;
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,20 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
+import React from "react";
+import ReactDOM from "react-dom";
 import css from "../css/app.css"
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Courses from "./Courses";
+const client = new ApolloClient({
+  uri: "https://vm8mjvrnv3.lp.gql.zone/graphql"
+});
+const Index = () => (
+  <ApolloProvider client={client}>
+    <div className="container">
+      <div>
+        <Courses />
+      </div>
+    </div>
+  </ApolloProvider>
+);
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
-import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+ReactDOM.render(<Index />, document.getElementById("index"));
