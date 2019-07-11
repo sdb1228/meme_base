a83113960887f9deae59bb3cf1990963f9f0102c
Cameron Matheson <cameron.matheson@gmail.com>
2019-07-11 14:00:39 -0700

    add proper meme-centric schema

--- lib/meme_base/schema.ex
+++ lib/meme_base/schema.ex
@@ -1,10 +1,18 @@
 defmodule MemeBase.Schema do
   use Absinthe.Schema
 
+  object :meme do
+    field :id, :id
+    field :url, :string
+  end
+
   query do
-    field :hello, :string do
+    field :memes, list_of(:meme) do
       resolve fn (_, _)->
-        {:ok, "Hello"}
+        {:ok, [
+          %{id: "1", url: "whatever.com/meme"},
+          %{id: "3", url: "memebase.inseng.net/vanilla-milk-shake"},
+        ]}
       end
     end
   end
