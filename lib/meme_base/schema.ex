ad058e1cfac66a8251434ac1d59c4a3ed8320bcd
Cameron Matheson <cameron.matheson@gmail.com>
2019-07-13 10:48:18 -0700

    paginate memes

--- lib/meme_base/schema.ex
+++ lib/meme_base/schema.ex
@@ -1,15 +1,20 @@
 defmodule MemeBase.Schema do
   use Absinthe.Schema
+  use Absinthe.Relay.Schema, :modern
+
+  alias MemeBase.{Meme, Repo}
 
   object :meme do
     field :id, :id
     field :url, :string
   end
 
+  connection node_type: :meme
+
   query do
-    field :memes, list_of(:meme) do
-      resolve fn (_, _)->
-        {:ok, MemeBase.Meme |> MemeBase.Repo.all}
+    connection field :memes_connection, node_type: :meme do
+      resolve fn (args, _) ->
+        Absinthe.Relay.Connection.from_query(Meme, &Repo.all/1, args)
       end
     end
   end
--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule MemeBase.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:absinthe, "~> 1.4.0"},
       {:absinthe_plug, "~> 1.4.0"},
+      {:absinthe_relay, "~> 1.4.0-rc or ~> 1.4"},
     ]
   end
 
