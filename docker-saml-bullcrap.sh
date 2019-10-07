2a9a234eb54dbd77f39b4dd69d8a4b533ab4cd17
Steven Burnett <sburnett@instructure.com>
2019-10-07 12:01:02 -0600

    allow better dev for mac

--- docker-saml-bullcrap.sh
+++ docker-saml-bullcrap.sh
@@ -2,4 +2,5 @@ docker run -p 8080:8080 -p 8443:8443 \
   -e SIMPLESAMLPHP_SP_ENTITY_ID=urn:memebase:fake-okta \
   -e SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE=http://localhost:4000/sso/sp/consume/okta \
   -e SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE=http://localhost:4000/sso/sp/logout/okta \
+  -e VIRTUAL_HOST=simplysaml.docker \
   kristophjunge/test-saml-idp
