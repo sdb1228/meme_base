09e4173839a33dc9d68d4029a7ebe55897fb26cb
Cameron Matheson <cameron.matheson@gmail.com>
2019-10-07 17:45:42 -0600

    temporarily disable csrf protection :(

--- lib/meme_base_web/router.ex
+++ lib/meme_base_web/router.ex
@@ -5,7 +5,7 @@ defmodule MemeBaseWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
+    #plug :protect_from_forgery TODO: uncomment this
     plug :put_secure_browser_headers
   end
 
