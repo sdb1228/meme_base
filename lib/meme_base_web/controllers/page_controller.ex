fea911568431995561a2ef1024b54229e3681be7
Cameron Matheson <cameron.matheson@gmail.com>
2019-10-08 13:30:39 -0600

    fix warning

--- lib/meme_base_web/controllers/page_controller.ex
+++ lib/meme_base_web/controllers/page_controller.ex
@@ -6,7 +6,7 @@ defmodule MemeBaseWeb.PageController do
   action_fallback MemeBaseWeb.FallbackController
 
   def index(conn, _params) do
-    with {:ok, saml_junk} <- Authenticator.authenticate(conn) do
+    with {:ok, _saml_junk} <- Authenticator.authenticate(conn) do
       render(conn, "index.html")
     end
   end
