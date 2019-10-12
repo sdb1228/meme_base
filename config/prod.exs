4369c30f9e7201914b3353794f0bae7b5da5f36d
Cameron Matheson <cameron.matheson@gmail.com>
2019-10-11 22:59:46 -0600

    fix prod port/hostname

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :meme_base, MemeBaseWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "memebase.inseng.net", port: 8080],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
